463dbafa5904a68e1cd960f98576f74140aa5010
Hariom Vishwakarma <hariom.learner@gmail.com>
2022-11-20 15:16:10 +0530

    container renaming and nginx reverse proxy setup

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="9sAt44ksct9KVX9mOA5kRU07unDoaJIQuKetKfXX3EGIh2Cpy1nvSQiuazaVDRXf",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "crypto-tracker.local"]
 
 # CACHES
 # ------------------------------------------------------------------------------
